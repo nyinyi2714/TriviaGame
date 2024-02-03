b89b8a27757856c21f5f7233cdb76dc33087e570
Nyi Nyi Moe Htet <68096773+nyinyi2714@users.noreply.github.com>
2024-02-03 15:53:55 -0800

    remove unused variables

--- vite-project/src/pages/HomePage/HomePage.tsx
+++ vite-project/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
 // HomePage.tsx
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 
 
 function HomePage() {
--- vite-project/src/pages/LoginPage/LoginPage.tsx
+++ vite-project/src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useAuth } from '../../hooks'
 import './LoginPage.css';
 
