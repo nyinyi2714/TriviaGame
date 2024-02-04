4e79fdc742006472d6a51e591705cb1724a5fcf9
greg <greg87peterson@gmail.com>
2024-02-03 19:52:27 -0800

    fixing ui

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,17 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
-import { HomePage, LoginPage } from './pages'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import { HomePage, LoginPage } from "./pages";
+import { Header } from "./components";
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='app'>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signin' element={<LoginPage />} />
-      </Routes>
+    <div className="container-app">
+      <div className="game-head">
+        <Header header={"FixTitle"} />
+      </div>
+      <div className="game-shell">
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signin" element={<LoginPage />} />
+          </Routes>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite-project/src/pages/HomePage/HomePage.tsx
+++ vite-project/src/pages/HomePage/HomePage.tsx
@@ -1,29 +1,27 @@
 // HomePage.tsx
-import { useState, useEffect } from 'react'
-import './HomePage.css'
-
-
+import { useState, useEffect } from "react";
+import "./HomePage.css";
 
 function HomePage() {
-
   return (
-    <div>
-      <h1>Trivia Game</h1>
-    </div>
-    <div className='homepage'>
-      <h1>Trivia Game</h1>
-      <h2>WHO WANTS TO BE A MILLIONAIRE</h2>
-
-      <div className='homepage-button'>
-        <button>NEW</button>
-        <button>RESUME</button>
-        <button>LEADERBOARD</button>
-        <button>SETTINGS</button>
-        <button>LOGOUT</button>
+    <div className="homepage-button">
+      <div className="new-box">
+        <button className="homeBtn">NEW</button>
+      </div>
+      <div className="resume-box">
+        <button className="homeBtn">RESUME</button>
+      </div>
+      <div className="leader-box">
+        <button className="homeBtn">LEADERBOARD</button>
+      </div>
+      <div className="settings-box">
+        <button className="homeBtn">SETTINGS</button>
+      </div>
+      <div className="logout-box">
+        <button className="homeBtn">LOGOUT</button>
       </div>
     </div>
-    
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
